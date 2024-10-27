855e89a927f48fd4db3c7705d082e80284043407
hugo garcia <hgarcia@audisoft.com>
2024-10-27 09:04:45 -0500

    migration change number of port

--- Migrations/20241027135809_ChangePortRedirectUri.Designer.cs
+++ Migrations/20241027135809_ChangePortRedirectUri.Designer.cs
@@ -4,6 +4,7 @@ using BookingServiceBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookingServiceBackend.Migrations
 {
     [DbContext(typeof(BookingContext))]
-    partial class BookingContextModelSnapshot : ModelSnapshot
+    [Migration("20241027135809_ChangePortRedirectUri")]
+    partial class ChangePortRedirectUri
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20241027135809_ChangePortRedirectUri.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingServiceBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePortRedirectUri : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AuthRedirectUrl",
+                value: "http://localhost:61770/callback");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Companies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AuthRedirectUrl",
+                value: "http://localhost:5173/callback");
+        }
+    }
+}
