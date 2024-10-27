fc50d6fcfd20abe860eb1d14603102070dd9b46d
hugo garcia <hgarcia@audisoft.com>
2024-10-26 21:47:35 -0500

    feat: added scopeds, transients and connection default to database

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using BookingServiceBackend.Data;
+using BookingServiceBackend.Middleware;
+using BookingServiceBackend.Repositories;
+using BookingServiceBackend.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<TokenValidationMiddleware>();
+
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
+builder.Services.AddScoped<IServicioRepository, ServicioRepository>();
+builder.Services.AddScoped<IReservaRepository, ReservaRepository>();
+
+builder.Services.AddTransient<ClienteService>();
+builder.Services.AddTransient<ServicioService>();
+builder.Services.AddTransient<ReservaService>();
+
+builder.Services.AddDbContext<BookingContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("Default"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("Default"))));
 
 var app = builder.Build();
 
@@ -19,6 +38,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseMiddleware<TokenValidationMiddleware>();
 
 app.MapControllers();
 
---
+++ Repositories/ICompanyRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace BookingServiceBackend.Repositories
+{
+    public interface ICompanyRepository
+    {
+    }
+}
